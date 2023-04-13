79e7d0dc9fb9bff97791d09990a10af03e0180b5
TDHM <tdhm-git@outlook.com>
2023-04-14 00:23:58 +0200

    Fix unit test by adding AvailableLanguages class

--- tests/test_translator.py
+++ tests/test_translator.py
@@ -1,8 +1,9 @@
+from subtitles_translator.available_languages import AvailableLanguages
 from subtitles_translator.translator import Translator
 
 
 def test_translate():
-    translator = Translator(source_language="en", target_language="fr")
+    translator = Translator(source_language=AvailableLanguages("en"), target_language=AvailableLanguages("fr"))
     source_test = "Hello World!"
     translated_test = translator.translate(source_test)
     assert translated_test == "Bonjour le monde!"
