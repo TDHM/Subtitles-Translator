23961b33a3fee5e99a2d42cbad91ec2620710960
TDHM <tdhm-git@outlook.com>
2023-04-13 21:13:31 +0200

    Add srt input/output mode

--- subtitles_translator/cli.py
+++ subtitles_translator/cli.py
@@ -12,29 +12,62 @@ if __name__ == "__main__":
         description="Translate the subtitles of the source movie and save a new movie file with translated subtitles."
     )
     parser.add_argument(
-        "-i", "--input", type=str, required=True, help="path to the input movie containing subtitles to be translated"
+        "-i",
+        "--input",
+        type=str,
+        required=True,
+        help=(
+            "path to the input movie (if ends with .mp4) or srt files (if ends with .srt) containing subtitles to be"
+            " translated"
+        ),
     )
     parser.add_argument(
-        "-o", "--output", type=str, required=True, help="desired path to the output movie with translated subtitles"
+        "-o",
+        "--output",
+        type=str,
+        required=True,
+        help=(
+            "desired path to the output movie (if ends with .mp4 and input is a movie) or srt files (if ends with .srt)"
+            " containing translated subtitles"
+        ),
     )
     parser.add_argument(
-        "-s", "--source", type=str, default="en", help="language of the input subtitles (source language)"
+        "-s", "--source", type=str, required=True, help="language of the input subtitles (source language)"
     )
-    parser.add_argument("-t", "--target", type=str, default="fr", help="target language")
+    parser.add_argument("-t", "--target", type=str, required=True, help="target language")
+    # this behavior is not implemented yet
     parser.add_argument(
-        "--copy", action="store_true", help="output a copy of the movie (default) or add translated subtitles in place"
+        "--copy",
+        action="store_true",
+        help=(
+            "output a copy of the movie (default) or add translated subtitles in place. no effects if input is a SRT"
+            " file"
+        ),
     )
     args = parser.parse_args()
 
-    source_srt = extract_srt(args.input).split("\n")
+    if args.input.endswith(".mp4"):
+        source_srt = extract_srt(args.input).split("\n")
+    elif args.input.endswith(".srt"):
+        if args.output.endswith(".mp4"):
+            raise ValueError("MP4 output file inconsistent with SRT file input.")
+        with open(args.input, "r+") as srt_file:
+            source_srt = srt_file.read().splitlines()
+    else:
+        raise ValueError("Input file format not supported.")
 
     translator = Translator(source_language=args.source, target_language=args.target)
     subtitles = Subtitles(source_srt)
 
     translator.translate_subtitles(subtitles)
-    subtitles.save_srt()
 
-    insert_srt(args.input, args.output, "translated.srt")
+    if args.output.endswith(".mp4"):
+        subtitles.save_srt()
+        insert_srt(args.input, args.output, "translated.srt")
+    elif args.output.endswith(".srt"):
+        subtitles.save_srt(args.output)
+    else:
+        raise ValueError("Input file format not supported.")
 
     end = time.time()
     print("Time elapsed: ", end - start)
