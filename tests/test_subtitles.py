8068fc468eee76a321d0c29204a10bc8b4e8e95c
TDHM <tdhm-git@outlook.com>
2023-05-15 12:13:24 +0200

    Complete tests for translator and subtitles classes

--- tests/test_subtitles.py
+++ tests/test_subtitles.py
@@ -2,6 +2,8 @@ from __future__ import annotations
 
 import pytest
 
+from subtitles_translator.subtitles import Subtitles
+
 
 @pytest.fixture
 def raw_subtitles() -> list[str]:
@@ -22,3 +24,23 @@ Test 4
     raw_subtitles = raw_str.split("\n")
 
     return raw_subtitles
+
+
+@pytest.fixture
+def subtitles(raw_subtitles) -> Subtitles:
+    subtitles = Subtitles(raw_subtitles)
+
+    return subtitles
+
+
+def test_build_aggregated_dico(subtitles):
+    assert subtitles.full_text_lines[3] == "Test 1"
+    assert subtitles.aggregated_dico_lines["Test 1"] == [[3]]
+    assert subtitles.aggregated_text_lines[0] == "Test 1"
+
+
+def test_save_srt(subtitles):
+    subtitles.save_srt("test.srt")
+    with open("test.srt", encoding="utf-8") as file:
+        content = file.read().splitlines()
+        assert content[3] == "Test 1"
