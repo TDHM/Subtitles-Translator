9d1c1b427e8a489ed4c4b35eac81e4c894eafa55
TDHM <tdhm-git@outlook.com>
2023-08-24 00:09:47 +0200

    Change model initialization

--- subtitles_translator/api_translate.py
+++ subtitles_translator/api_translate.py
@@ -1,24 +1,25 @@
-from subtitles_translator.translator_gcp import Translator
-
 from fastapi import FastAPI
 from pydantic import BaseModel
+
+from subtitles_translator.translator_gcp import Translator
+
 
 class SourceText(BaseModel):
     text: str = ""
 
+
+translator = Translator()
+
 app = FastAPI()
 
-@app.on_event("startup")
-def load_model():
-    print("initializing translator...")
-    translator = Translator()
-    print("done")
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
+
 # response = requests.get("http://localhost:8000/translate/", json={"text":"Hello world!"})
+# curl -X GET localhost:8000/translate -H 'Content-Type: application/json' -d '{"text":"Hello world!"}'
 @app.get("/translate")
 def translate(source_text: SourceText = None):
     translated_text = translator.translate(source_text.text)
